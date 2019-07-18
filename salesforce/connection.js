21b5ec2ea5f878f297267ea38a73976e107ecc88
Chandra Nayak <cmohan.nayak@gmail.com>
2019-07-17 19:17:52 -0700

    Process ENV added

--- salesforce/connection.js
+++ salesforce/connection.js
@@ -3,7 +3,7 @@ const nforce = require('nforce');
 /**
  * Creates connection to Salesforce CRM
  */
-/*
+
 module.exports = nforce.createConnection({
   clientId: '3MVG9oNqAtcJCF.HJ7Os.EETzcC.4G11ggh4bQNmMMwiAuWPMeZZgycgN3iNVsA4cykVjxlbqH5hB4Jtl4b8F',
   clientSecret: '80F9F3AAA5104995E59AE29F764DA4D4A8A0C6C0D1020C4A9614A1E3FA5B304E',
@@ -17,8 +17,8 @@ module.exports = nforce.createConnection({
     access_token: '6Cel800Df2000001LNlk888f2000000TqdJKgpHJ24d6lgWRdQM6tscyKO0fmcET72T6ab4yKMDh8G8AfZFodi2qrxJncp8esPWrE5i3gJO',
     instance_url: 'https://chandra-dev.my.salesforce.com',
   },
-}); */
-
+}); 
+/*
 module.exports = nforce.createConnection({
   clientId: process.env.SF_CLIENT_ID,
   clientSecret: process.env.SF_CLIENT_SECRET,
@@ -32,5 +32,5 @@ module.exports = nforce.createConnection({
     access_token: process.env.SF_ACCESS_TOKEN,
     instance_url: process.env.SF_INSRANCE_URL,
   },
-});
+});*/
 
